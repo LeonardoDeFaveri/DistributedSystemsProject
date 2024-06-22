0644008378b4c7b852f50719b3c3b799bad9b45d
Lorenzo <lorenzofumi@gmail.com>
2024-06-22 13:50:01 +0200

    Fix to the ElectionTest

--- src/test/java/it/unitn/ds1/ElectionTest.java
+++ src/test/java/it/unitn/ds1/ElectionTest.java
@@ -6,6 +6,8 @@ import it.unitn.ds1.models.administratives.JoinGroupMsg;
 import it.unitn.ds1.models.administratives.StartMsg;
 import it.unitn.ds1.models.election.ElectionMsg;
 import it.unitn.ds1.models.update.WriteMsg;
+import it.unitn.ds1.models.update.WriteOkMsg;
+import it.unitn.ds1.utils.UpdateRequestId;
 import it.unitn.ds1.utils.WriteId;
 
 import java.io.IOException;
@@ -23,7 +25,7 @@ public class ElectionTest {
 
         for (int i = 0; i < N_REPLICAS; i++) {
             // Initially the coordinator is the first created replica
-            replicas.add(system.actorOf(Replica.props(i, 0, -1), "replica" + i));
+            replicas.add(system.actorOf(Replica.props(i, 0, 0), "replica" + i));
         }
 
         for (ActorRef replica : replicas) {
@@ -31,13 +33,15 @@ public class ElectionTest {
             replica.tell(new StartMsg(), ActorRef.noSender());
         }
 
+        var client = system.actorOf(Client.props(replicas), "client");
+
         var values = new int[]{1, 2, 3, 4, 5};
 
         // First, let them add the messages to the list
         for (int i = 0; i < values.length; i++) {
             WriteId id = new WriteId(0, i);
             for (var replica : replicas) {
-                replica.tell(new WriteMsg(null, id, values[i]), null);
+                replica.tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
             }
         }
 
@@ -46,27 +50,32 @@ public class ElectionTest {
         // 0 has all the updates
         for (int i = 0; i < values.length; i++) {
             WriteId id = new WriteId(0, i);
-            replicas.get(2).tell(new WriteMsg(null, id, values[i]), null);
+            replicas.get(2).tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
+            replicas.get(2).tell(new WriteOkMsg(new WriteId(0, i), new UpdateRequestId(client, i)), null);
         }
 
         // 1 and 2 have one less update
         for (int i = 0; i < values.length - 1; i++) {
             WriteId id = new WriteId(0, i);
-            replicas.get(1).tell(new WriteMsg(null, id, values[i]), null);
-            replicas.get(0).tell(new WriteMsg(null, id, values[i]), null);
+            replicas.get(1).tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
+            replicas.get(0).tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
+            replicas.get(0).tell(new WriteOkMsg(new WriteId(0, i), new UpdateRequestId(client, i)), null);
+            replicas.get(1).tell(new WriteOkMsg(new WriteId(0, i), new UpdateRequestId(client, i)), null);
         }
         // 3 and 4 have two less updates
         for (int i = 0; i < values.length - 2; i++ ){
             WriteId id = new WriteId(0, i);
-            replicas.get(3).tell(new WriteMsg(null, id, values[i]), null);
-            replicas.get(4).tell(new WriteMsg(null, id, values[i]), null);
+            replicas.get(3).tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
+            replicas.get(4).tell(new WriteMsg(new UpdateRequestId(client, i), id, values[i]), null);
+            replicas.get(4).tell(new WriteOkMsg(new WriteId(0, i), new UpdateRequestId(client, i)), null);
+            replicas.get(3).tell(new WriteOkMsg(new WriteId(0, i), new UpdateRequestId(client, i)), null);
         }
 
         // Then, we start the election algorithm
         replicas.get(0).tell(new ElectionMsg(0, 4, new WriteId(0, 2)), replicas.get(4));
 
         // Required to see all output
-        //while (replicas.size() > 0) {}
+        while (replicas.size() > 0) {}
 
         System.out.printf(">>> Press ENTER <<<\n");
         try {
